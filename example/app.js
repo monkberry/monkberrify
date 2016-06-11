80874e1ce5e0542191663188b7c057f1da55e980
Anton Medvedev <anton@medv.io>
2016-06-12 00:20:32 +0700

    Release 4.0.0

--- example/app.js
+++ example/app.js
@@ -1,5 +1,3 @@
 var monkberry = require('../node_modules/monkberry/monkberry');
-monkberry.mount(require('./view.monk'));
-
-var view = monkberry.render('view', {name: 'world'});
-document.body.appendChild(view.createDocument());
+var template = require('./view.monk');
+var view = monkberry.render(template, document.body);
